ce1ae2dbe46c8681401a8374bf1cea338472dc2d
Noble0505 <143568699+Noble0505@users.noreply.github.com>
2023-10-26 19:45:15 -0600

    Add files via upload

---
+++ EVA2_13_CICLO_FOR/src/eva2_13_ciclo_for/EVA2_13_CICLO_FOR.java
@@ -0,0 +1,23 @@
+
+package eva2_13_ciclo_for;
+
+
+public class EVA2_13_CICLO_FOR {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+    }
+        System.out.println("------------");
+        for(int i = 9; i >= 0; i--){
+            System.out.println(i);
+    }
+        System.out.println("------------");
+        for(int i = 0; i <=100; i+= 2){
+            System.out.print(i + " - ");
+    }
+    }
+    
+}
